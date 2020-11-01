face6f2fc9f5f71fef3ec26813235ff44d31f7a1
developer-t2 <oo_ri@naver.com>
2020-11-01 20:05:44 +0900

    production update

--- client/config/index.js
+++ client/config/index.js
@@ -1,2 +1,2 @@
 export const backend =
-  process.env.NODE_ENV === 'production' ? 'http://dev-t2.com' : 'http://localhost:3065';
+  process.env.NODE_ENV === 'production' ? 'http://api.dev-t2.com' : 'http://localhost:3065';
